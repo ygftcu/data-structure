115a941d98764d613ca6e573f2583a540f676de5
tridevi <118823921+ygftcu@users.noreply.github.com>
2025-01-15 18:03:27 +0900

    gyegui
    재귀

---
+++ hanoi.py
@@ -0,0 +1,19 @@
+import sys
+
+def input() :
+    return sys.stdin.readline()
+
+n = int(input())
+def top(n, a, b, c):
+    if n == 1:
+        print(a, c)
+    else:
+        top(n-1, a, c, b)
+        print(a, c)
+        top(n-1, b, a, c)
+
+sum = 2 ** n - 1
+
+print(sum)
+
+top(n, 1, 2, 3)
